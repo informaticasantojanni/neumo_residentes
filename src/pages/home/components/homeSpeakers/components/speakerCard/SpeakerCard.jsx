dbb677123559c8f3ec211d1f8be216b58c5bad13
jnardi86 <julian.nardi@gmail.com>
2024-06-27 12:28:45 -0300

    Add styles to speacker section

--- src/pages/home/components/homeSpeakers/components/speakerCard/SpeakerCard.jsx
+++ src/pages/home/components/homeSpeakers/components/speakerCard/SpeakerCard.jsx
@@ -10,24 +10,21 @@ const SpeakerCard = ({ name: name, speciality: speciality, review: review, image
   }
 
   return (
-    <div className="w-[90%] md:w-[60%] laptop:w-[80%] h-[550px] mx-auto mb-5 flex flex-col items-center justify-top bg-Mint-green border-solid border-4 border-White rounded-xl">
-      <div
-        className="w-full h-64 pt-1 flex items-center justify-center"
-      >
+    <div className="w-[90%] md:w-[60%] laptop:w-[80%] h-[550px] mx-auto mb-5 flex flex-col items-center justify-top bg-Mint-green border-solid border-4 border-White rounded-xl desktop:group-hover:bg-Navy-blue">
+      <div className="w-full h-64 pt-1 flex items-center justify-center">
         <img src={image} className="object-cover rounded-full w-52 h-52" alt="Speaker Image" />
       </div>
       <div className="w-full mb-5 p-5 flex flex-col justify-center items-center rounded-xl md:px-2 md:py-5">
-        <h3 className="text-xl p-2 font-montserrat font-Bold text-Navy-blue md:text-2xl">{name}</h3>
-        <h5 className="text-base font-montserrat font-Semibold mb-3 text-Navy-blue desktop: md:text-xl">{speciality}</h5>
+        <h3 className="text-xl p-2 font-montserrat font-Bold text-Navy-blue md:text-2xl desktop:group-hover:text-White">{name}</h3>
+        <h5 className="text-base font-montserrat font-Semibold mb-3 text-Navy-blue desktop:group-hover:text-White md:text-xl">{speciality}</h5>
         {/* {showFullReview ? review : `${review.slice(0, 150)}...`} */}
         {/* <p className="font-montserratAlternate font-Regular text-sm text-start leading-relaxed md:text-xl text-White">
             {review} </p> */}
         {review.map((item, index) => (
-          <p key={index} className="font-montserratAlternate font-Regular text-sm text-center leading-relaxed md:text-xl text-Navy-blue">
+          <p key={index} className="font-montserratAlternate font-Regular text-sm text-center leading-relaxed md:text-xl text-Navy-blue desktop:group-hover:text-White">
             {item}
           </p>
         ))}
-
         <div>
           {/* <div className="w-full flex justify-start">
             <HeaderButton
@@ -38,10 +35,7 @@ const SpeakerCard = ({ name: name, speciality: speciality, review: review, image
             />
           </div> */}
         </div>
-
-
       </div>
-
     </div>
   )
 }
--- src/pages/home/components/homeSpeakers/view/SpeakerView.jsx
+++ src/pages/home/components/homeSpeakers/view/SpeakerView.jsx
@@ -6,27 +6,27 @@ import SpeakerCarouselCase3 from "../components/speakerCard/SpeakerCarouselCase3
 const SpeakerView = () => {
   return (
     <div>
-      <h3 className='font-montserrat font-Bold text-4xl text-center mb-10 text-Navy-blue desktop:group-hover:text-Blue desktop:text-5xl'>DISERTANTES</h3>
+      <h3 className='font-montserrat font-Bold text-4xl text-center mb-10 text-Navy-blue desktop:group-hover:text-Navy-blue desktop:text-5xl'>DISERTANTES</h3>
       <div className="w-full mx-auto mb-40 flex flex-col bg-Navy-blue group desktop:hover:bg-Mint-green rounded-xl shadow-md md:h-[730px]">
         <div className="w-full text-center p-1">
-          <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Blue desktop:text-xl'>CASO CLINICO 1</h6>
-          <p className="font-montserrat font-Semibold text-2xl mb-5 text-White desktop:group-hover:text-Blue desktop:text-2xl">ASMA: LO QUE NO DICEN LAS GUÍAS</p>
+          <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Navy-blue desktop:text-xl'>CASO CLINICO 1</h6>
+          <p className="font-montserrat font-Semibold text-lg mb-5 text-White desktop:group-hover:text-Navy-blue desktop:text-2xl">ASMA: LO QUE NO DICEN LAS GUÍAS</p>
         </div>
         <SpeakerCarouselCase1 />
       </div>
 
       <div className="w-full mx-auto mb-40 flex flex-col bg-Navy-blue group desktop:hover:bg-Mint-green rounded-xl shadow-md md:h-[730px]">
         <div className="w-full text-center p-1">
-        <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Blue desktop:text-xl'>CASO CLINICO 2</h6>
-        <p className="font-montserrat font-Semibold text-2xl mb-5 text-White desktop:group-hover:text-Blue desktop:text-2xl">HIPERTENSIÓN PULMONAR: CUANDO LA HP. PUEDE TENER VARIAS CAUSAS</p>
+        <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Navy-blue desktop:text-xl'>CASO CLINICO 2</h6>
+        <p className="font-montserrat font-Semibold text-lg mb-5 text-White desktop:group-hover:text-Navy-blue desktop:text-2xl">HIPERTENSIÓN PULMONAR: CUANDO LA HP. PUEDE TENER VARIAS CAUSAS</p>
         </div>
         <SpeakerCarouselCase2 />
       </div>
 
       <div className="w-full mx-auto mb-40 flex flex-col bg-Navy-blue group desktop:hover:bg-Mint-green rounded-xl shadow-md md:h-[730px]">
         <div className="w-full text-center p-1">
-        <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Blue desktop:text-xl'>CASO CLINICO 3</h6>
-        <p className="font-montserrat font-Semibold text-2xl mb-5 text-White desktop:group-hover:text-Blue desktop:text-2xl">ENFERMEDAD INTERSTICIAL: RECORRIDO DEL DIAGNOSTICO AL TRASPLANTE</p>
+        <h6 className='font-montserrat font-Regular text-base mb-5 text-Soft-red desktop:group-hover:text-Navy-blue desktop:text-xl'>CASO CLINICO 3</h6>
+        <p className="font-montserrat font-Semibold text-lg mb-5 text-White desktop:group-hover:text-Navy-blue desktop:text-2xl">ENFERMEDAD INTERSTICIAL: RECORRIDO DEL DIAGNOSTICO AL TRASPLANTE</p>
         </div>
         <SpeakerCarouselCase3 />
       </div>
